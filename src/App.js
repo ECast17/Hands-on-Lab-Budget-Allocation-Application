71404c4e6c8398248bb4c972eb1dcae5e6587099
ECast17 <144755147+ECast17@users.noreply.github.com>
2024-02-21 08:18:09 -0500

    Update App.js
    added expensetotal component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import Budget from './components/Budget';
 
 // Add code to import the other components here under
 import Remaining from './components/Remaining';
-
+import ExpenseTotal from './components/ExpenseTotal';
 import { AppProvider } from './context/AppContext';
+
 const App = () => {
     return (
         <AppProvider>
@@ -30,7 +31,10 @@ const App = () => {
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                            // ExpenseTotal
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                                </div>
                         }        
                        
                         {
